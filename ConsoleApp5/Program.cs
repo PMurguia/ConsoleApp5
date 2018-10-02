035db9ce892cf94e7df455cd41392d9a8c8834dc
Peio Murguia <peiomurguia@gmail.com>
2018-10-02 14:13:38 +0200

    Otro cambio más

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -18,7 +18,7 @@ namespace ConsoleApp5
 
             Console.WriteLine(8 / 2);
             Console.ReadLine();
-            
+
             Console.WriteLine(-5 + (8 * 6));
 
 
@@ -29,78 +29,78 @@ namespace ConsoleApp5
             Console.WriteLine((5 + (15 / (3 * 2)) - 8) % 3);
             Console.ReadLine();
 
-               int n1;
-               int n2;
-               Console.WriteLine("Introduce un número: ");
-               n1 = Int32.Parse(Console.ReadLine());
-               Console.WriteLine("Introduce otro número: ");
-                n2 = Int32.Parse(Console.ReadLine());
-                Console.WriteLine("El resultado de multiplicar " + n1 + " por " + n2 + " es " + (n1 * n2));
+            int n1;
+            int n2;
+            Console.WriteLine("Introduce un número: ");
+            n1 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Introduce otro número: ");
+            n2 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("El resultado de multiplicar " + n1 + " por " + n2 + " es " + (n1 * n2));
 
-                float num1;
-                float num2;
-                Console.WriteLine("Introduce un número: ");
-                num1 = Int32.Parse(Console.ReadLine());
-                Console.WriteLine("Introduce otro número: ");
-                num2 = Int32.Parse(Console.ReadLine());
-                Console.WriteLine("El resultado de multiplicar " + num1 + " por " + num2 + " es " + (num1 * num2));
-                Console.WriteLine("El resultado de sumar " + num1 + " y " + num2 + " es " + (num1 + num2));
-                Console.WriteLine("El resultado de restar " + num1 + " y " + num2 + " es " + (num1 - num2));
-                Console.WriteLine("El resultado de dividir " + num1 + " entre " + num2 + " es " + (num1 / num2));
-                Console.WriteLine("El resto de la división entre " + num1 + " y " + num2 + " es " + (num1 % num2));
+            float num1;
+            float num2;
+            Console.WriteLine("Introduce un número: ");
+            num1 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Introduce otro número: ");
+            num2 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("El resultado de multiplicar " + num1 + " por " + num2 + " es " + (num1 * num2));
+            Console.WriteLine("El resultado de sumar " + num1 + " y " + num2 + " es " + (num1 + num2));
+            Console.WriteLine("El resultado de restar " + num1 + " y " + num2 + " es " + (num1 - num2));
+            Console.WriteLine("El resultado de dividir " + num1 + " entre " + num2 + " es " + (num1 / num2));
+            Console.WriteLine("El resto de la división entre " + num1 + " y " + num2 + " es " + (num1 % num2));
 
-                int numMulti;
-                Console.WriteLine("Introduce un número.");
-                numMulti = Int32.Parse(Console.ReadLine());
-                Console.WriteLine(numMulti + " x 1 = " + numMulti * 1);
-                Console.WriteLine(numMulti + " x 2 = " + numMulti * 2);
-                Console.WriteLine(numMulti + " x 3 = " + numMulti * 3);
-                Console.WriteLine(numMulti + " x 4 = " + numMulti * 4);
-                Console.WriteLine(numMulti + " x 5 = " + numMulti * 5);
-                Console.WriteLine(numMulti + " x 6 = " + numMulti * 6);
-                Console.WriteLine(numMulti + " x 7 = " + numMulti * 7);
-                Console.WriteLine(numMulti + " x 8 = " + numMulti * 8);
-                Console.WriteLine(numMulti + " x 9 = " + numMulti * 9);
-                Console.WriteLine(numMulti + " x 10 = " + numMulti * 10);
+            int numMulti;
+            Console.WriteLine("Introduce un número.");
+            numMulti = Int32.Parse(Console.ReadLine());
+            Console.WriteLine(numMulti + " x 1 = " + numMulti * 1);
+            Console.WriteLine(numMulti + " x 2 = " + numMulti * 2);
+            Console.WriteLine(numMulti + " x 3 = " + numMulti * 3);
+            Console.WriteLine(numMulti + " x 4 = " + numMulti * 4);
+            Console.WriteLine(numMulti + " x 5 = " + numMulti * 5);
+            Console.WriteLine(numMulti + " x 6 = " + numMulti * 6);
+            Console.WriteLine(numMulti + " x 7 = " + numMulti * 7);
+            Console.WriteLine(numMulti + " x 8 = " + numMulti * 8);
+            Console.WriteLine(numMulti + " x 9 = " + numMulti * 9);
+            Console.WriteLine(numMulti + " x 10 = " + numMulti * 10);
 
-                Console.WriteLine("El resultado de  ((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5)) es " + ((25.5 * 3.5 - (3.5 * 3.5)) / (40.5 - 4.5)));
+            Console.WriteLine("El resultado de  ((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5)) es " + ((25.5 * 3.5 - (3.5 * 3.5)) / (40.5 - 4.5)));
 
-                Console.WriteLine("El resultado de (4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11))) es " + (4.0 * (1 - (1.0 / 3) + (1.0 / 5) - (1.0 / 7) + (1.0 / 9) - (1.0 / 11))));
+            Console.WriteLine("El resultado de (4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11))) es " + (4.0 * (1 - (1.0 / 3) + (1.0 / 5) - (1.0 / 7) + (1.0 / 9) - (1.0 / 11))));
 
-                float radio;
-                const double PI = 3.141593;
-                Console.WriteLine("Introduce un número. ");
-                radio = Int32.Parse(Console.ReadLine());
-                Console.WriteLine("El área del círculo es " + (PI * (radio * radio)));
-                Console.WriteLine("El perímetro del círculo es " + (2 * radio * PI));
+            float radio;
+            const double PI = 3.141593;
+            Console.WriteLine("Introduce un número. ");
+            radio = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("El área del círculo es " + (PI * (radio * radio)));
+            Console.WriteLine("El perímetro del círculo es " + (2 * radio * PI));
 
-                int primNum;
-                int secNum;
-                int tercNum;
-                Console.WriteLine("Introduzca un número.");
-                primNum = Int32.Parse(Console.ReadLine());
-                Console.WriteLine("Introduzca un número.");
-                secNum = Int32.Parse(Console.ReadLine());
-                Console.WriteLine("Introduzca un número.");
-                tercNum = Int32.Parse(Console.ReadLine());
-                Console.WriteLine("La media de los tres números es " + ((primNum + secNum + tercNum) / 3));
+            int primNum;
+            int secNum;
+            int tercNum;
+            Console.WriteLine("Introduzca un número.");
+            primNum = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Introduzca un número.");
+            secNum = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Introduzca un número.");
+            tercNum = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("La media de los tres números es " + ((primNum + secNum + tercNum) / 3));
 
-                int altura;
-                int anchura;
-                Console.WriteLine("Altura de cuadrado. ");
-                altura = Int32.Parse(Console.ReadLine());
-                Console.WriteLine("Anchura del cuadrado. ");
-                anchura = Int32.Parse(Console.ReadLine());
-                Console.WriteLine("El perímetro del cuadrado es " + (2 * (altura + anchura)));
-                Console.WriteLine("El área del cuadrado es " + (altura * anchura));
+            int altura;
+            int anchura;
+            Console.WriteLine("Altura de cuadrado. ");
+            altura = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Anchura del cuadrado. ");
+            anchura = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("El perímetro del cuadrado es " + (2 * (altura + anchura)));
+            Console.WriteLine("El área del cuadrado es " + (altura * anchura));
 
-                int var1;
-                int var2;
-                Console.WriteLine("Escribe un número. ");
-                var1 = Int32.Parse(Console.ReadLine());
-                Console.WriteLine("Escribe otro. ");
-                var2 = Int32.Parse(Console.ReadLine());
-                Console.WriteLine("El valor del primer número es " + var2 + " y el valor del segundo es " + var1);
+            int var1;
+            int var2;
+            Console.WriteLine("Escribe un número. ");
+            var1 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Escribe otro. ");
+            var2 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("El valor del primer número es " + var2 + " y el valor del segundo es " + var1);
 
             int numero1;
             int numero2;
@@ -131,31 +131,31 @@ namespace ConsoleApp5
             Console.ReadLine();
 
 
-            //Pendiente comparación numeros
+           // Pendiente comparación numeros
 
             int numero;
-                Console.WriteLine("Introduce un número de 3 digitos. ");
-                numero = Int32.Parse(Console.ReadLine());
-                int digito1 = numero % 10;
-                int digito2 = (numero % 100 - digito1) / 10;
-                int digito3 = numero / 100;
-                Console.WriteLine("Digito1 es: " + digito1);
-                Console.WriteLine("Digito2 es: " + digito2);
-                Console.WriteLine("Digito3 es: " + digito3);
-                Console.WriteLine("La suma de los dígitos del número " + numero + " es " + (numero % 10 + (numero % 100 - digito1) / 10 + numero / 100));
-                Console.ReadLine();
+            Console.WriteLine("Introduce un número de 3 digitos. ");
+            numero = Int32.Parse(Console.ReadLine());
+            int digito1 = numero % 10;
+            int digito2 = (numero % 100 - digito1) / 10;
+            int digito3 = numero / 100;
+            Console.WriteLine("Digito1 es: " + digito1);
+            Console.WriteLine("Digito2 es: " + digito2);
+            Console.WriteLine("Digito3 es: " + digito3);
+            Console.WriteLine("La suma de los dígitos del número " + numero + " es " + (numero % 10 + (numero % 100 - digito1) / 10 + numero / 100));
+            Console.ReadLine();
 
-                int digito;
-                Console.WriteLine("Dame un dígito. ");
-                digito = Int32.Parse(Console.ReadLine());
-                Console.WriteLine("Primero uno, luego dos, después tres y sumamos al final. ");
-                Console.WriteLine(digito);
-                Console.WriteLine(digito * 11);
-                Console.WriteLine(digito * 111);
-                Console.WriteLine("El total es " + (digito + digito * 11 + digito * 111));
-                Console.ReadLine();
+            int digito;
+            Console.WriteLine("Dame un dígito. ");
+            digito = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Primero uno, luego dos, después tres y sumamos al final. ");
+            Console.WriteLine(digito);
+            Console.WriteLine(digito * 11);
+            Console.WriteLine(digito * 111);
+            Console.WriteLine("El total es " + (digito + digito * 11 + digito * 111));
+            Console.ReadLine();
 
-            //Escribe un programa que calcule el resto de dividir dos números sin utilizar el operador de división de resto (%)
+           // Escribe un programa que calcule el resto de dividir dos números sin utilizar el operador de división de resto(%)
             int dig1;
             int dig2;
             Console.WriteLine("Dame un número. ");
@@ -166,42 +166,42 @@ namespace ConsoleApp5
             Console.WriteLine(-(((dig1 / dig2) * dig2 - dig1)));
             Console.ReadLine();
 
-            //Escribe un programa que le pida al usuario un número de pulgadas y lo convierta a metros (una pulgada son 0.0254 metros)
+           // Escribe un programa que le pida al usuario un número de pulgadas y lo convierta a metros(una pulgada son 0.0254 metros)
 
             int pulgadas;
             const double conver = 0.0254;
-                Console.WriteLine("Dame una medida en pulgadas. Intentare pasarla a metros. ");
-                pulgadas = Int32.Parse(Console.ReadLine());
-                Console.WriteLine("Eso son " + pulgadas * conver + " metros. ");
-                Console.ReadLine();
+            Console.WriteLine("Dame una medida en pulgadas. Intentare pasarla a metros. ");
+            pulgadas = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Eso son " + pulgadas * conver + " metros. ");
+            Console.ReadLine();
 
 
-                //Escribe un programa que pida un número de minutos y muestre cuantos años y días son
+           // Escribe un programa que pida un número de minutos y muestre cuantos años y días son
 
-                float minutos;
-                Console.WriteLine("Dime un número de minutos, y lo pasaremos a días primero y a años después. ");
-                minutos = Int32.Parse(Console.ReadLine());
-                Console.WriteLine("Eso son " + (minutos / 60 / 24) + " días, o " + minutos / 60 / 24 / 365 + " años ");
-                Console.ReadLine();
+            float minutos;
+            Console.WriteLine("Dime un número de minutos, y lo pasaremos a días primero y a años después. ");
+            minutos = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Eso son " + (minutos / 60 / 24) + " días, o " + minutos / 60 / 24 / 365 + " años ");
+            Console.ReadLine();
 
 
-                //Escribe un programa que pida una distancia (en metros), y el tiempo tardado (como tres números: horas, minutos y segundos)
-                //y que muestre la velocidad en metros por segundo y kilómetros por hora.
+           // Escribe un programa que pida una distancia(en metros), y el tiempo tardado(como tres números: horas, minutos y segundos)
+          //  y que muestre la velocidad en metros por segundo y kilómetros por hora.
 
-                float metros;
-                float hour;
-                float min;
-                float sec;
-                Console.WriteLine("¿Qué distancia has recorrido? ");
-                metros = Int32.Parse(Console.ReadLine());
-                Console.WriteLine("¿En cuántas horas? ");
-                hour = Int32.Parse(Console.ReadLine());
-                Console.WriteLine("¿En cuántos minutos? ");
-                min = Int32.Parse(Console.ReadLine());
-                Console.WriteLine("¿Y segundos? ");
-                sec = Int32.Parse(Console.ReadLine());
-                Console.WriteLine("Has recorrido " + metros * sec + " metros por segundo, o lo que es lo mismo, " + (metros / 1000) * hour + " kilómetros por hora. ");
-                Console.ReadLine();
+            float metros;
+            float hour;
+            float min;
+            float sec;
+            Console.WriteLine("¿Qué distancia has recorrido? ");
+            metros = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("¿En cuántas horas? ");
+            hour = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("¿En cuántos minutos? ");
+            min = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("¿Y segundos? ");
+            sec = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Has recorrido " + metros * sec + " metros por segundo, o lo que es lo mismo, " + (metros / 1000) * hour + " kilómetros por hora. ");
+            Console.ReadLine();
 
 
             /**Escribe un programa que pida al usuario 3 números. 
@@ -216,29 +216,123 @@ namespace ConsoleApp5
             primerNum = Int32.Parse(Console.ReadLine());
             segundoNum = Int32.Parse(Console.ReadLine());
             tercerNum = Int32.Parse(Console.ReadLine());
-            
+            if (primerNum + segundoNum == tercerNum)
+            {
+                Console.WriteLine("La suma de " + primerNum + " y " + segundoNum + " es igual a " + tercerNum);
+            }
+            else
+            {
+                Console.WriteLine("La suma de " + primerNum + " y " + segundoNum + " no es igual a " + tercerNum);
+            }
+            Console.ReadLine();
+
+            //Una hora = 3.600 segundos
+
+            int segundos;
+            Console.WriteLine("Pasemos segundos a horas, minutos y segundos. ");
+            segundos = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Eso son " + segundos / 3600 + " horas, " + (segundos - 3600) / 60 + " minutos y " + ((segundos - 3600) % 60) + " segundos.");
+            Console.ReadLine();
 
 
 
 
+            //Escribe un programa que pida al usuario tres números y que diga si uno de ellos es 20 menos que uno de los otros.
 
+            int otroNum1;
+            int otroNum2;
+            int otroNum3;
+            Console.WriteLine("Dame tres números diferentes entre si. ");
+            otroNum1 = Int32.Parse(Console.ReadLine());
+            otroNum2 = Int32.Parse(Console.ReadLine());
+            otroNum3 = Int32.Parse(Console.ReadLine());
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            
+            if (otroNum1 == otroNum2 - 20 || otroNum2 == otroNum1 - 20)
+            {
+                if (otroNum1 == otroNum2 - 20)
+                {
+                    Console.WriteLine(otroNum2 + " es 20 menos que " + otroNum1);
+                }
+                else if (otroNum2 == otroNum1 - 20)
+                {
+                    Console.WriteLine(otroNum1 + " es 20 menos que " + otroNum2);
+                }
+                else
+                {
+                    Console.WriteLine("Gracias. ");
+                }
+            }
+            if (otroNum2 == otroNum3 - 20 || otroNum3 == otroNum2 - 20)
+            {
+                if (otroNum2 == otroNum3 - 20)
+                {
+                    Console.WriteLine(otroNum3 + " es 20 menos que " + otroNum2);
+                }
+                else if (otroNum3 == otroNum2 - 20)
+                {
+                    Console.WriteLine(otroNum2 + " es 20 menos que " + otroNum3);
+                }
+                else
+                {
+                    Console.WriteLine("Gracias. ");
+                }
+            }
+            if (otroNum3 == otroNum1 - 20 || otroNum1 == otroNum3 - 20)
+            {
+                if (otroNum3 == otroNum1 - 20)
+                {
+                    Console.WriteLine(otroNum1 + " es 20 menos que " + otroNum3);
+                }
+                else if (otroNum1 == otroNum3 - 20)
+                {
+                    Console.WriteLine(otroNum3 + " es 20 menos que " + otroNum1);
+                }
+                else
+                {
+                    Console.WriteLine("Gracias. ");
+               }
+            }
+            else
+            {
+                Console.WriteLine("Gracias. ");
+            }
+            Console.ReadLine();
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
